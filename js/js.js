b86080d722756a7f8531aa52812a1c38ada9446b
taynaradb <taynaraax0@hotmail.com>
2021-05-02 22:11:07 -0300

    Ex three

--- js/js.js
+++ js/js.js
@@ -1,18 +1,20 @@
+
+   //EXERCICO 1
+
 function ex01() {
     alert("Alo, Mundo!");
 }
 
+   //EXERCICO 1
 
-
-   
+   //EXERCICO 2
    function ex02() {
-
-    let ex = document.getElementById('ex2');
+    let ex2 = document.getElementById('ex2');
      
-    if(ex.style.display == 'block'){
-        ex.style.display = 'none';
+    if(ex2.style.display == 'block'){
+        ex2.style.display = 'none';
     }else{
-        ex.style.display = "block";
+        ex2.style.display = "block";
     }
 }
 
@@ -24,7 +26,30 @@ if(!num){
 }else{
   document.getElementById("res2").innerHTML = `O número informado foi ${num}`;
 }
-
 }
 
+   //EXERCICO 2
 
+   //EXERCICO 3
+
+   function ex03() {
+    let ex3 = document.getElementById('ex3');
+     
+    if(ex3.style.display == 'block'){
+        ex3.style.display = 'none';
+    }else{
+        ex3.style.display = "block";
+    }
+}
+
+    function display3(){
+        let num1 = document.getElementById("ex03num").value;
+        let num2 = document.getElementById("ex03num2").value;
+        let total = parseInt(num1) + parseInt(num2);
+
+        if(!num1 || !num2){
+            alert("Favor informar um número");
+        }else{
+        document.getElementById("res3").innerHTML = `A resposta de ${num1} e ${num2} é de ${total}`;
+        }
+    }
